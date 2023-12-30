fcbc41f2b382f81bd0d2fd2188a97d570071dd16
khan Mohammad Talha <khanmohammadtalha@khans-MacBook-Air.local>
2023-12-30 20:56:11 +0600

    investing page added

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ function showMoreServices() {
   const upToDownArrow = document.getElementById("upArrow");
   upToDownArrow.style = "transform: rotate(180deg);";
 }
+
+document.getElementById("buying").addEventListener("click", function () {
+  window.location.href = "buying.html";
+});
+document.getElementById("selling").addEventListener("click", function () {
+  window.location.href = "selling.html";
+});
+document.getElementById("selling2").addEventListener("click", function () {
+  window.location.href = "selling.html";
+});
